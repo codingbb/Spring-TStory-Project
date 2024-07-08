11c562f86a54206c15ea3ba863163b0c55a912d8
SCHcoding <codingb007@gmail.com>
2024-07-09 00:03:19 +0900

    Init: Category 생성

---
+++ src/main/java/site/metacoding/blogv3/category/CategoryController.java
@@ -0,0 +1,12 @@
+package site.metacoding.blogv3.category;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+
+@RequiredArgsConstructor
+@Controller
+public class CategoryController {
+    private final CategoryService categoryService;
+
+
+}
---
+++ src/main/java/site/metacoding/blogv3/category/CategoryJPARepository.java
@@ -0,0 +1,8 @@
+package site.metacoding.blogv3.category;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CategoryJPARepository extends JpaRepository<Category, Integer> {
+
+
+}
