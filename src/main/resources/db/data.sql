2aabdf146da87b2772bfce455a5774f028887efd
SCHcoding <codingb007@gmail.com>
2024-07-12 22:13:35 +0900

    Data: 더미 수정

--- src/main/resources/db/data.sql
+++ src/main/resources/db/data.sql
@@ -1,10 +1,10 @@
 -- 유저 더미
-INSERT INTO user_tb (username, password, email, email_confirm, created_at)
-VALUES ('ssar', '1234', 'ssar@nate.com', true, now());
-INSERT INTO user_tb (username, password, email, email_confirm, created_at)
-VALUES ('cos', '1234', 'cos@nate.com', true, now());
-INSERT INTO user_tb (username, password, email, email_confirm, created_at)
-VALUES ('love', '1234', 'love@nate.com', true, now());
+INSERT INTO user_tb (username, password, email, created_at)
+VALUES ('ssar', '1234', 'ssar@nate.com', now());
+INSERT INTO user_tb (username, password, email, created_at)
+VALUES ('cos', '1234', 'cos@nate.com', now());
+INSERT INTO user_tb (username, password, email, created_at)
+VALUES ('love', '1234', 'love@nate.com', now());
 -- 유저 더미 끝
 
 -- 카테고리 더미 (it도 넣기)
