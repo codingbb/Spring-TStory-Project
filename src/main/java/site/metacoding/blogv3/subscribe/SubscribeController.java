8e072732f68047122ab0089c5691cabc6452e16c
SCHcoding <codingb007@gmail.com>
2024-07-18 10:48:04 +0900

    Design: footer 고정

--- src/main/java/site/metacoding/blogv3/subscribe/SubscribeController.java
+++ src/main/java/site/metacoding/blogv3/subscribe/SubscribeController.java
@@ -3,6 +3,7 @@ package site.metacoding.blogv3.subscribe;
 import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import site.metacoding.blogv3.user.User;
 
@@ -11,6 +12,12 @@ import site.metacoding.blogv3.user.User;
 public class SubscribeController {
     private final SubscribeService subscribeService;
     private final HttpSession session;
+
+    @GetMapping("/sub/list")
+    public String list() {
+
+        return "/subscribe/subscribeList";
+    }
 
 
     @PostMapping("/sub/save")
