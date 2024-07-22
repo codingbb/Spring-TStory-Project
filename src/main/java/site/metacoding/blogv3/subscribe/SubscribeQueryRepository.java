0f8a91979e5a11c072e4c3f48100e051e09a0f80
SCHcoding <codingb007@gmail.com>
2024-07-22 12:43:35 +0900

    Fix: 최신글 구독 DESC 처리

--- src/main/java/site/metacoding/blogv3/subscribe/SubscribeQueryRepository.java
+++ src/main/java/site/metacoding/blogv3/subscribe/SubscribeQueryRepository.java
@@ -14,22 +14,38 @@ public class SubscribeQueryRepository {
     private final EntityManager em;
 
     public List<Object[]> subscribeList(Integer fromUserId) {
+//        String q = """
+//                SELECT post_id, title, user_id, u.username
+//                        FROM (SELECT p.id AS post_id, p.title, p.user_id, s.to_user_id,
+//                                ROW_NUMBER() OVER (PARTITION BY p.user_id ORDER BY p.id DESC) AS rn
+//                                FROM post_tb p
+//
+//                                JOIN subscribe_tb s
+//                                ON p.user_id = s.to_user_id
+//                                WHERE s.from_user_id = ?
+//
+//                                ORDER BY p.user_id DESC) AS subquery
+//                        INNER JOIN user_tb u
+//                        ON subquery.to_user_id = u.id
+//                        WHERE rn <= 2
+//                        ORDER BY subquery.user_id DESC, post_id DESC
+//                """;
+
         String q = """
                 SELECT post_id, title, user_id, u.username
-                        FROM (SELECT p.id AS post_id, p.title, p.user_id, s.to_user_id,
+                        FROM (SELECT p.id AS post_id, p.title, p.user_id, s.to_user_id, s.id,
                                 ROW_NUMBER() OVER (PARTITION BY p.user_id ORDER BY p.id DESC) AS rn
                                 FROM post_tb p
-                                
+                            
                                 JOIN subscribe_tb s
                                 ON p.user_id = s.to_user_id
-                                WHERE s.from_user_id = ?
-                                
-                                ORDER BY p.user_id DESC) AS subquery
+                                WHERE s.from_user_id = ?) AS subquery
+
                         INNER JOIN user_tb u
                         ON subquery.to_user_id = u.id
                         WHERE rn <= 2
-                        ORDER BY subquery.user_id DESC, post_id DESC
-                """;
+                        ORDER BY subquery.id DESC, post_id DESC
+                   """;
 
         Query query = em.createNativeQuery(q);
         query.setParameter(1, fromUserId);
